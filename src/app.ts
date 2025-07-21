80c1e5a4849bd3eed44304694895d7de49d79781
dev7-ngenit <dev7.ngenit@gmail.com>
2025-07-21 17:02:28 +0600

    add: created global error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 
 const app: Application = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to the Assignment 3, Library Management System!");
+  res.json({
+    success: true,
+    message: "Welcome to the Assignment 3, Library Management System API!",
+  });
+});
+
+// Error Middleware
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  res.status(500).json({
+    message: error?.message || "An unexpected error occurred.",
+    success: false,
+    error: {
+      name: error.name || "Unknown Error",
+    },
+  });
 });
 
 export default app;
