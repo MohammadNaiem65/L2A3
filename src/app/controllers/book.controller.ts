99f59d4fabe09f7b056d3ce69351f3c5fa9530ff
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-07-21 23:55:54 +0600

    add: created /api/books get api

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -1,9 +1,54 @@
 import express, { Request, Response } from "express";
+import { PipelineStage } from "mongoose";
 import Book from "../models/book.model";
 
 const bookRouter = express.Router();
 
 const requiredFields = ["title", "author", "genre", "isbn", "copies"];
+
+bookRouter.get("/", async (req: Request, res: Response) => {
+  const { filter, sort, sortby, limit = 10 } = req.query;
+
+  let pipeline: PipelineStage[] = [];
+
+  if (filter) {
+    pipeline.push({
+      $match: {
+        genre: filter,
+      },
+    });
+  }
+  if (sortby) {
+    pipeline.push({
+      $sort: {
+        [sortby as string]: sort === "desc" ? -1 : 1,
+      },
+    });
+  }
+  pipeline.push({
+    $limit: parseInt(limit as string),
+  });
+
+  try {
+    const books = await Book.aggregate(pipeline);
+
+    return res.status(200).json({
+      success: true,
+      message: "Books fetched successfully",
+      data: books,
+    });
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+      error: {
+        name: "ServerError",
+        message: "An error occurred while fetching books",
+      },
+    });
+  }
+});
 
 bookRouter.post("/", async (req: Request, res: Response) => {
   // Check for required fields
