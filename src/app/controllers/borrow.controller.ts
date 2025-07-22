5aff88c88430b2799438b940da94421ac7dcfc9b
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-07-22 17:37:50 +0600

    update: created /api/borrow api to get borrowed book summery

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -4,6 +4,71 @@ import Borrow from "../models/borrow.model";
 
 const borrowRouter = express.Router();
 
+// Borrowed book summary
+borrowRouter.get("/", async (req: Request, res: Response) => {
+  const { page = "1", limit = "10" } = req.query;
+
+  const _page = parseInt(page as string);
+  const _limit = parseInt(limit as string);
+
+  const pipeline = [
+    {
+      $group: {
+        _id: "$book",
+        totalQuantity: {
+          $sum: "$quantity",
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "books",
+        localField: "_id",
+        foreignField: "_id",
+        as: "book",
+      },
+    },
+    {
+      $unwind: {
+        path: "$book",
+      },
+    },
+    {
+      $project: {
+        book: {
+          title: "$book.title",
+          isbn: "$book.isbn",
+        },
+        _id: 0,
+        totalQuantity: 1,
+      },
+    },
+    {
+      $skip: (_page >= 1 ? _page - 1 : 0) * _limit,
+    },
+  ];
+
+  try {
+    const response = await Borrow.aggregate(pipeline);
+
+    res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: response,
+    });
+  } catch (error: any) {
+    console.error(
+      "Error fetching borrowed book summary:",
+      JSON.stringify(error, null, 2)
+    );
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching borrowed book summary.",
+      error: error.message,
+    });
+  }
+});
+
 // Borrow a book
 borrowRouter.post("/", async (req: Request, res: Response) => {
   const { book, quantity, dueDate } = req.body;
